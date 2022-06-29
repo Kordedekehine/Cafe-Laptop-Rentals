7e8aee4375b31690880efe76d62acaacbe48441c
amigoscode <salamikehinde345@gmail.com>
2022-06-29 12:25:58 +0100

    initial setup

---
+++ src/main/java/movieRentage/rentages/MovierentagesApplication.java
@@ -0,0 +1,13 @@
+package movieRentage.rentages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovierentagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovierentagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/movieRentage/rentages/MovierentagesApplicationTests.java
@@ -0,0 +1,13 @@
+package movieRentage.rentages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovierentagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
